6a7e79f5936d28ac07646518a4b22dad1c5ff7d5
myricseptember <myric@zulzi.com>
2021-02-10 13:20:31 +0200

    add Login screen

--- starter/app/src/main/java/com/udacity/shoestore/MainActivity.kt
+++ starter/app/src/main/java/com/udacity/shoestore/MainActivity.kt
@@ -1,14 +1,34 @@
 package com.udacity.shoestore
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
+import com.udacity.shoestore.databinding.ActivityMainBinding
 import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var activityMainBinding: ActivityMainBinding
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        activityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         Timber.plant(Timber.DebugTree())
+
+        appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.loginFragment
+            )
+        )
+
+        setSupportActionBar(activityMainBinding.toolbar)
+        navController = findNavController(R.id.nav_host_Fragment)
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration)
     }
 }
---
+++ starter/app/src/main/java/com/udacity/shoestore/ui/viewmodel/ShoeStoreViewModel.kt
@@ -0,0 +1,6 @@
+package com.udacity.shoestore.ui.viewmodel
+
+import androidx.lifecycle.ViewModel
+
+class ShoeStoreViewModel : ViewModel() {
+}
